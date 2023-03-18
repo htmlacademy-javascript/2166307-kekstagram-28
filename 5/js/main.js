f453d5a7c0cc02c3fbc8907964f76b94fb33db08
Keks <keks@htmlacademy.ru>
2023-03-18 17:47:02 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,11 @@
+import './form-master.js';
+import './photo-editor.js';
+import './photo-viewer.js';
+import './upload-master.js';
+import './view-filter.js';
+import {createDescriptionArr} from './testdata-generator.js';
+
+// eslint-disable-next-line no-console
+console.log(createDescriptionArr(10));
+
+
