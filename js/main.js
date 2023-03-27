2879bdc126fefee136cb6ed8f36dd29434f9b4c8
AndrewPlatonov888 <very@blacky.ru>
2023-03-27 10:19:57 +0300

    module8-task1 | homework

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-import './form-master.js';
-import './photo-editor.js';
-import './photo-viewer.js';
-import './upload-master.js';
-import './view-filter.js';
 
 import { createDescriptionArr } from './testdata-generator.js';
 import { renderThumbnails } from './thumbnails.js';
+import { openThumbnail } from './thumbnails-enlarger.js';
+import { closeThumbnail} from './thumbnails-enlarger.js';
 
-renderThumbnails(createDescriptionArr(25));
+// генерируем тестовые данные
+const thumbnails = createDescriptionArr(26);
+renderThumbnails(thumbnails);
+
+//хендлер на открытие модального окна по клику на миниатюре (вешаем на секцию)
+const thumbnailsSection = document.querySelector('.pictures');
+thumbnailsSection.addEventListener('click', openThumbnail);
+
+//хендлер на закрытие модального окна по клику мышкой на крестик
+const closeElement = document.querySelector('.big-picture__cancel');
+closeElement.addEventListener('click', closeThumbnail);
+
+export {
+  thumbnails
+};
